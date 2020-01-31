34487c1368f7faf277f6558f68f8d0ec1c1e639a
UxDeveloper82 <electronicsmf@aol.com>
2020-01-30 22:19:47 -0500

    Add project files.

---
+++ ApplianceRepair/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApplianceRepair.Startup))]
+namespace ApplianceRepair
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
