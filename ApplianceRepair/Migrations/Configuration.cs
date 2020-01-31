99a08b12d23d444c5ae218e3761c7481b067a876
UxDeveloper82 <electronicsmf@aol.com>
2020-01-30 23:35:47 -0500

    First Commit

---
+++ ApplianceRepair/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ApplianceRepair.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplianceRepair.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ApplianceRepair.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
